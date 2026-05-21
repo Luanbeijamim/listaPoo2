3e262f03b45a72a7bfce1b0791951528db4d7a4f
Luanbeijamim <luan.b.cunha@aluno.senai.br>
2026-05-21 19:08:31 -0400

    exercicio2

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,13 +1,16 @@
 package principal;
 
-import wrappers.Exercicio01;
+import wrappers.Exercicio02;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("--- Executando Exercício 1 (Wrappers) ---");
+        System.out.println("=== Executando Lista de POO ===");
 
-        Exercicio01 ex1 = new Exercicio01();
-        ex1.executar();
+        // Se quiser rodar o exercício 1:
+        // Exercicio01 ex1 = new Exercicio01();
+        // ex1.executar();
+        Exercicio02 ex2 = new Exercicio02();
+        ex2.executar();
     }
 }
